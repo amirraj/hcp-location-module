851dc7882700a08e9c0ee065d0cf8dcc982f5589
Amir Hossain Raj <Amir.Raj@brainstation23.com>
2021-04-13 19:13:55 +0600

    basic structure

---
+++ src/hcp-list.component.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+
+function HCPs(props) {
+    return <h1>Get HCPs for facility :, {props.facility}</h1>;
+  }
+export default HCPs;
---
+++ src/location-list.component.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+function Locations(props) {
+    return <h1>Selected Trial :, {props.trialID}</h1>;
+  }
+export default Locations;
+
